4fc3f5b5906e5519783d92edb359b2943d92b4e4
saleemsangi <greatsangi@yahoo.com>
2013-06-02 17:46:44 -0500

    Initial Commit

---
+++ Sindh4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sindh4
+//
+//  Created by Saleem Sangi on 6/2/13.
+//  Copyright (c) 2013 Ignas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sindh4/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Sindh4
+//
+//  Created by Saleem Sangi on 6/2/13.
+//  Copyright (c) 2013 Ignas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
